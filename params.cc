62d98c70ebd8780c46e631d4fd47eb3fbb713512
Robert J. Peters <rjpcal@gmail.com>
2000-02-03 23:07:04 +0000

    Moved macro definitions of GET*, PUT*, and PRINT* from .h file into .c file.

--- params.cc
+++ params.cc
@@ -4,7 +4,7 @@
 // Rob Peters rjpeters@klab.caltech.edu
 //   created by Achim Braun
 // created: Tue Feb  1 16:30:51 2000
-// written: Tue Feb  1 16:38:46 2000
+// written: Thu Feb  3 15:04:29 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -57,6 +57,27 @@ void process_id( char pid[] );
 
 
 /************************************************/
+
+#define GETINT(name) {fgets(line,120,fp);sscanf(line,"%s %d",text,&(name));}
+#define GETCHAR(name) {fgets(line,120,fp);sscanf(line,"%s %c",text,&(name));}
+#define GETFLOAT(name) {fgets(line,120,fp);sscanf(line,"%s %f",text,&(name));}
+#define GETTEXT(name) {fgets(line,120,fp);sscanf(line,"%s %s",text,(name));}
+#define GETINTL(name) {fgets(line,120,fp);sscanf(line,"%s %d %d %d %d",text,&(name)[0],&(name)[1],&(name)[2],&(name)[3]);}
+#define GETTEXTL(name) {fgets(line,120,fp);sscanf(line,"%s %s %s %s %s",text,(name)[0],(name)[1],(name)[2],(name)[3]);}
+
+#define PUTINT(name,text) {fprintf(fp,"%-19s %d\n",(text),(name));}
+#define PUTCHAR(name,text) {fprintf(fp,"%-19s %c\n",(text),(name));}
+#define PUTFLOAT(name,text) {fprintf(fp,"%-19s %.2f\n",(text),(name));}
+#define PUTTEXT(name,text) {fprintf(fp,"%-19s %+s\n",(text),(name));}
+#define PUTINTL(name,text) {fprintf(fp,"%-19s %d %d %d %d\n",(text),(name)[0],(name)[1],(name)[2],(name)[3]);}
+#define PUTTEXTL(name,text) {fprintf(fp,"%-19s %+s %+s %+s %+s\n",(text),(name)[0],(name)[1],(name)[2],(name)[3]);}
+
+#define PRINTINT(name,text) {printf( "%-19s %d\n",(text),(name));}
+#define PRINTCHAR(name,text) {printf( "%-19s %c\n",(text),(name));}
+#define PRINTFLOAT(name,text) {printf( "%-19s %.2f\n",(text),(name));}
+#define PRINTTEXT(name,text) {printf( "%-19s %+s\n",(text),(name));}
+#define PRINTINTL(name,text) {printf( "%-19s %d %d %d %d\n",(text),(name)[0],(name)[1],(name)[2],(name)[3]);}
+#define PRINTTEXTL(name,text) {printf( "%-19s %+s %+s %+s %+s\n",(text),(name)[0],(name)[1],(name)[2],(name)[3]);}
 
 void ReadParams( char extension[] )
 {
