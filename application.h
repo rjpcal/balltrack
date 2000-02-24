9cab858987dec3084bf128155e559a653f91e238
Robert J. Peters <rjpcal@gmail.com>
2000-02-24 00:48:44 +0000

    Moved private, X11-related stuff into impl struct XStuff.

--- application.h
+++ application.h
@@ -3,7 +3,7 @@
 // application.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Tue Feb 22 20:06:46 2000
-// written: Wed Feb 23 15:54:26 2000
+// written: Wed Feb 23 16:44:28 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -11,12 +11,15 @@
 #ifndef APPLICATION_H_DEFINED
 #define APPLICATION_H_DEFINED
 
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
-
-#include "defs.h"
+#ifndef _XLIB_H_
+typedef union _XEvent XEvent;
+#endif
 
 class Application {
+private:
+  Application(const Application&);
+  Application& operator=(const Application&);
+
 public:
   Application(int argc, char** argv);
   virtual ~Application();
@@ -44,16 +47,7 @@ protected:
   virtual void onMenuChoice(char c);
 
 private:
-  void openDisplay();
-  void createVisual();
-  void createColormap();
-  void createWindow();
-  void selectInput();
   void mapImageWindow();
-  void setWmProperty();
-  void setWmProtocol();
-  void windowInfo();
-  void wrapWindow();
 
   void keyPressAction( XEvent* event );
 
@@ -62,17 +56,13 @@ private:
   int itsArgc;
   char** itsArgv;
 
-  Display* itsDisplay;
-  int itsScreen;
-  int itsFildes;
-  int itsDepth;
   int itsWidth;
   int itsHeight;
-  char itsName[STRINGSIZE];
-  Window itsWindow;
-  Visual* itsVisual;
-  Colormap itsColormap;
-  XColor itsMeanColor;
+
+  int itsFildes;
+
+  struct XStuff;
+  XStuff* itsXStuff;
 };
 
 static const char vcid_application_h[] = "$Header$";
