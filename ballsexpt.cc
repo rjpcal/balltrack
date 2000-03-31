125c63c834de96e4fc4ae858fb74ebb567ff7799
Robert J. Peters <rjpcal@gmail.com>
2000-03-31 01:28:16 +0000

    Added runFixationCalibration() and added different APPLICATION_MODE's.

--- ballsexpt.cc
+++ ballsexpt.cc
@@ -3,7 +3,7 @@
 // ballsexpt.cc
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Feb 23 15:41:51 2000
-// written: Wed Mar  8 09:03:36 2000
+// written: Thu Mar 30 17:27:57 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -54,6 +54,28 @@ DOTRACE("BallsExpt::fillMenu");
   }
 }
 
+void BallsExpt::runFixationCalibration() {
+DOTRACE("BallsExpt::runFixationCalibration");
+
+  int w = graphics()->width();
+  int h = graphics()->height();
+
+  int x[] = { w/2, 50, w/2, w-50,  50, w/2, w-50,   50,  w/2, w-50, w/2 };
+  int y[] = { h/2, 50,  50,   50, h/2, h/2,  h/2, h-50, h-50, h-50, h/2 };
+
+  int seq[] = { 0, 2, 4, 6, 8, 1, 3, 5, 7, 9, 10 };
+
+  for (int i = 0; i < 11; ++i)
+	 {
+		graphics()->writeAllPlanes();
+		graphics()->waitVerticalRetrace();
+		graphics()->clearFrontBuffer();
+		graphics()->drawCross(x[seq[i]], y[seq[i]]);
+		Timing::mainTimer.set();
+		Timing::mainTimer.wait( 1.0 );
+	 }
+}
+
 void BallsExpt::runExperiment() {
 DOTRACE("BallsExpt::runExperiment");
 
@@ -89,14 +111,38 @@ DOTRACE("BallsExpt::runExperiment");
   int cycle;
   for( cycle=0; cycle<CYCLE_NUMBER; ++cycle ) {
 
-	 // Run active tracking trial
-	 theBalls.runTrial(graphics(), &tp[timepoint++], Balls::CHECK_ALL);
+	 if (EYE_TRACKING == APPLICATION_MODE)
+		{
+		  // Run active tracking trial
+		  runFixationCalibration();
+		  theBalls.runTrial(graphics(), &tp[timepoint++], Balls::CHECK_ALL);
 
-	 // Run active tracking trial with objective check
-	 theBalls.runTrial(graphics(), &tp[timepoint++], Balls::CHECK_ONE);
+		  // Run active tracking trial with objective check
+		  runFixationCalibration();
+		  theBalls.runTrial(graphics(), &tp[timepoint++], Balls::CHECK_ONE);
 
-	 // Run passive trial
-  	 theBalls.runTrial(graphics(), &tp[timepoint++], Balls::PASSIVE);
+		  // Run passive trial
+		  runFixationCalibration();
+		  theBalls.runTrial(graphics(), &tp[timepoint++], Balls::PASSIVE);
+		}
+
+	 if (TRAINING == APPLICATION_MODE)
+		{
+		  // Run active tracking trial
+		  theBalls.runTrial(graphics(), &tp[timepoint++], Balls::CHECK_ALL);
+		  // Run active tracking trial with objective check
+		  theBalls.runTrial(graphics(), &tp[timepoint++], Balls::CHECK_ONE);
+		  // Run passive trial
+		  theBalls.runTrial(graphics(), &tp[timepoint++], Balls::PASSIVE);
+		}
+
+	 if (FMRI_SESSION == APPLICATION_MODE)
+		{
+		  // Run active tracking trial with objective check
+		  theBalls.runTrial(graphics(), &tp[timepoint++], Balls::CHECK_ONE);
+		  // Run passive trial
+		  theBalls.runTrial(graphics(), &tp[timepoint++], Balls::PASSIVE);
+		}
   }
 
   Timing::mainTimer.set();
