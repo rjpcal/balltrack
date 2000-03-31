0c6bdb83344a34dfef4ca410e763df2ba5a97fca
Robert J. Peters <rjpcal@gmail.com>
2000-03-31 00:43:16 +0000

    Added runFixationCalibration().

--- ballsexpt.h
+++ ballsexpt.h
@@ -3,7 +3,7 @@
 // ballsexpt.h
 // Rob Peters rjpeters@klab.caltech.edu
 // created: Wed Feb 23 15:41:20 2000
-// written: Tue Feb 29 16:12:38 2000
+// written: Thu Mar 30 16:43:09 2000
 // $Id$
 //
 ///////////////////////////////////////////////////////////////////////
@@ -20,6 +20,8 @@ public:
   BallsExpt(const XHints& hints);
 
 protected:
+  virtual void runFixationCalibration();
+
   virtual void runExperiment();
 
   virtual void fillMenu(char menu[][STRINGSIZE], int nitems);
